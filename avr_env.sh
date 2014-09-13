bb79641749410859ab5336a3be22869c1a23a626
Tam Do <aznoohwee85@gmail.com>
2014-09-13 12:56:33 -0500

    Add avr specific environment setup

---
+++ avr_env.sh
@@ -0,0 +1,14 @@
+if [ $PLATFORM == 'cygwin' ]; then
+    avrdude_release='avrdude-6.1-mingw32.zip'
+
+    if [ ! -f /usr/bin/avrdude.exe ]
+    then
+        cd /tmp
+        wget http://download.svannah.gnu.org/releases/avrdude/${avrdude_release}
+        unzip ${avrdude_release}
+        cp avrdude.exe /usr/bin
+        cp avrdude.conf /usr/bin
+    fi
+
+    cp .bash_avr ~/
+fi
